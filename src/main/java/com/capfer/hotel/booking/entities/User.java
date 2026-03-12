e6a2020972d227725ef527b988d502016c7a546e
capfer27 <capfer27@gmailcom>
2026-03-13 00:19:10 +0400

    add equals and hash code to user entity

--- src/main/java/com/capfer/hotel/booking/entities/User.java
+++ src/main/java/com/capfer/hotel/booking/entities/User.java
@@ -1,15 +1,12 @@
 package com.capfer.hotel.booking.entities;
 
-import com.capfer.hotel.booking.enums.BookingStatus;
 import com.capfer.hotel.booking.enums.UserRole;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-import org.antlr.v4.runtime.misc.NotNull;
-import org.aspectj.bridge.IMessage;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Setter
 @Getter
@@ -50,4 +47,28 @@ public class User {
 
     private Boolean isActive;
     private final LocalDateTime createdAt = LocalDateTime.now();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(phoneNumber, user.phoneNumber) && role == user.role && Objects.equals(isActive, user.isActive) && Objects.equals(createdAt, user.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, firstName, lastName, phoneNumber, role, isActive, createdAt);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", role=" + role +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
