2b10fb5cc198ba52b0ed333a08dc4f21919f8300
capfer27 <capfer27@gmailcom>
2026-03-29 16:31:12 +0400

    update oauth endpoint

--- HotelBooking/src/main/java/com/capfer/hotel/booking/controllers/AuthController.java
+++ HotelBooking/src/main/java/com/capfer/hotel/booking/controllers/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class AuthController {
 
